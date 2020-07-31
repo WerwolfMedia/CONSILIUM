d07941601e7407b7628b7893b7d4fb5e5547d791
Manuel Pfender <mail@mpfender.de>
2020-07-31 15:12:17 +0200

    debug
    Signed-off-by: Manuel Pfender <mail@mpfender.de>

--- login.php
+++ login.php
@@ -62,12 +62,14 @@ function login($username, $password)
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
   $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
   $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
- echo "test;";
+
   if (login($username, $password) == true) {
     // Login erfolgreich 
     header("Location: /index.php");
+    echo "erolfg";
   } else {
     // Login fehlgeschlagen 
+    echo "failed";
   }
 }
 include_once $_SERVER['DOCUMENT_ROOT'] . '/include/header.php';
