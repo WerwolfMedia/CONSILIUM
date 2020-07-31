88e8efea844782c704d74f4bce8e8bb93960718c
Manuel Pfender <mail@mpfender.de>
2020-07-31 16:34:01 +0200

    Menu
    Signed-off-by: Manuel Pfender <mail@mpfender.de>

--- include/menu.php
+++ include/menu.php
@@ -13,22 +13,22 @@
               <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-toggle="dropdown">
                 <span class="avatar" style="background-image: url(./static/avatars/000m.jpg)"></span>
                 <div class="d-none d-xl-block pl-2">
-                  <div><?php echo $_SESSION['vorname'];echo " ";echo $_SESSION['nachname'] ?> <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">  <polyline id="svg_1" stroke-width="2" stroke-miterlimit="10" stroke="#ffffff" points="21,8.5 12,17.5 3,8.5 " fill="none"/></svg></div>
+                  <div><?php echo $_SESSION['vorname'];echo " ";echo $_SESSION['nachname'] ?> </div>
                   <div class="mt-1 small text-muted"></div>
                 </div>
               </a>
               <div class="dropdown-menu dropdown-menu-right">
                 <a class="dropdown-item" href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" class="icon dropdown-item-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"/><path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" /><circle cx="12" cy="12" r="3" /></svg>
-                  Action
+                  Einstellungen
                 </a>
                 <a class="dropdown-item" href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" class="icon dropdown-item-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"/><path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" /><path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" /><line x1="16" y1="5" x2="19" y2="8" /></svg>
-                  Another action
+                  Hilfe
                 </a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#"><svg xmlns="http://www.w3.org/2000/svg" class="icon dropdown-item-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"/><line x1="12" y1="5" x2="12" y2="19" /><line x1="5" y1="12" x2="19" y2="12" /></svg>
-                  Separated link</a>
+                  Abmelden</a>
               </div>
             </div>
           </div>
