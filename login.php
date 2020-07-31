2a08f73d432ce9ebe79786941ba7874d58017555
Manuel Pfender <mail@mpfender.de>
2020-07-31 15:10:31 +0200

    Test
    Signed-off-by: Manuel Pfender <mail@mpfender.de>

--- login.php
+++ login.php
@@ -62,7 +62,7 @@ function login($username, $password)
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
   $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
   $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-
+ echo "test;";
   if (login($username, $password) == true) {
     // Login erfolgreich 
     header("Location: /index.php");
